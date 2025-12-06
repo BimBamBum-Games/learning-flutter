5a4d67ccebb98102a51ec5986d12d704c2616e6e
Haluk Özgen <haluk.ozgen90@gmail.com>
2025-12-07 01:01:47 +0300

    Add PopupMenuButton with DynamicList use

---
+++ FlutterPersonalWorks/flutter_study4/lib/ListViewCardPopupMenuButton/main_ListView_Card_PopupMenuButton.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<String> items = ["Axe", "Sword", "Knife", "Bow", "Shield"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(""),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (c, i) {
+          return Card(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(items[i]),
+                Spacer(),
+
+                //PopupMenuButton and Builder Template
+                PopupMenuButton(
+
+                  onSelected: (comingValue){
+                    if(comingValue == 0){
+                      print("Delete Triggered!");
+                    }
+                    if(comingValue == 1){
+                      print("Update Triggered!");
+                    }
+                  },
+
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                        value: 0,
+                        child: Text("Delete"),
+                      ),
+                      PopupMenuItem(
+                        value: 1,
+                        child: Text("Update"),
+                      )
+                    ];
+                  }
+
+                ),
+              ],
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
