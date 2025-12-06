b70dda2ada0f9fcbe713bcd1d69bdb439aba467a
Haluk Özgen <haluk.ozgen90@gmail.com>
2025-12-06 17:18:06 +0300

    Add AppBar with Search mechanics

--- FlutterPersonalWorks/flutter_study4/lib/AppBarSearch/main_AppBarSearch.dart
+++ FlutterPersonalWorks/flutter_study4/lib/AppBarSearch/main_AppBarSearch.dart
@@ -28,12 +28,43 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  //With a Boolean we can control the AppBar Search.
+  bool canSearch = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(""),
+        backgroundColor: Colors.blueAccent,
+        title: canSearch ?
+          Text("AppBar Search Application") :
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Search",
+            ),
+            onChanged: (value){
+              print("Search Value: $value");
+            },
+          ),
+        actions: [
+          canSearch ?
+          IconButton(
+              onPressed: (){
+                setState(() {
+                  canSearch = false;
+                });
+              },
+              icon: Icon(Icons.search)
+          ) :
+          IconButton(
+              onPressed: (){
+                setState(() {
+                  canSearch = true;
+                });
+              },
+              icon: Icon(Icons.cancel)
+          ),
+        ],
       ),
       body: Center(
         child: Column(
